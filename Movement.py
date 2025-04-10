1196624e1c0abb27e12001d0fbb1d7cc1d730d78
Diego Espindola <diegoespindola9404@gmail.com>
2025-04-10 14:49:30 -0300

    Create Movement.py
    Creating the class and testing

---
+++ Movement.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from datetime import date
+from enum import Enum
+
+class MovementType(Enum):
+    INCOME = "income"
+    EXPENSE = "expense"
+
+@dataclass
+class Movement:
+    """Here will be the financial movements"""
+    idMovement: int
+    value: float
+    description: str
+    idCategory: int
+    movDate: date
+    movType: MovementType
+
+    def __post_init__(self):
+        if not isinstance(self.movType, MovementType):
+            raise ValueError("The movType is not a instance of MovementType Enum")
+        
+    def insert(self):
+        print("inserting on the dataBase")
+
+
+    def find(id)
+    
+    @staticmethod
+    def update(self):
+        print(f"updating id {self.idMovement}")
+
+    @staticmethod
+    def delete(idMovement):
+        print(f"deleting id {idMovement}")
+
+
+Movement(500, "Salário", 1, date.today(), MovementType.INCOME).insert()
+Movement.update(500)
+Movement.delete(500)
+
+
+"""
+def __init__(self, value, description, idCategory, movDate, movType):
+        self.value = value
+        self.description = description
+        self.idCategory = idCategory
+        self.movDate = movDate
+        self.movType = movType
+"""
+
+    
+
